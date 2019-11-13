7be3909725f31601cbae8dbe9f63c0dded8f4adf
prakash <prakash>
2019-11-13 06:54:33 +0545

    first commit

---
+++ questans/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Answers,QuestionGroups,Questions
+
+admin.site.register(Answers)
+admin.site.register(Questions)
+admin.site.register(QuestionGroups)
